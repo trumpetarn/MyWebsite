9aedff004ad34311c7b5b1feebb50e850f85163f
Jakob Markgren <markgren@outlook.com>
2018-01-06 11:04:14 +0100

    Added wind and wdir

--- python/fetchTfeWeather.py
+++ python/fetchTfeWeather.py
@@ -8,15 +8,26 @@ def fetch_tfe_data(debug=False):
 		root = tree.getroot()
 		return root
 	data = urllib.request.urlopen("http://www8.tfe.umu.se/vadertjanst/service1.asmx/GetData").read()	
-	return data
+	return xml.fromstring(data)
 
 def get_temp(data):
 	temp = data.find('.//tempmed')
 	return float(temp.text)
 
 def get_wind(data):
-	temp = data.find('.//tempmed')
-	return float(temp.text)
+	hast = data.find('.//vindh')
+	rikt = data.find('.//vindr')
+	print(data.find('.//vindmax').text)
+	return float(hast.text),int(rikt.text)
 
-data = fetch_tfe_data()
+def wind_direction(deg):
+	DIR = ["NNO","NO","ONO","O","OSO","SO","SSO","S","SSV","SV","VSV","V","VNV","NV","NNV","N"]
+	N = len(DIR)
+	V = (360//N)//2
+	return DIR[(deg-V)//(2*V)]
+
+data = fetch_tfe_data(True)
 print(get_temp(data), '*C')
+v,d = get_wind(data)
+print(v,d)
+print(wind_direction(d))
